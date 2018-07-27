00b024fc391ed984a8b9f6e97778e6fc2d36e65e
gmy <296778322@qq.com>
2018-07-27 17:03:48 +0800

    <seed to private>

--- src/ecc/ecc_auth.c
+++ src/ecc/ecc_auth.c
@@ -11,13 +11,14 @@
 #define VERSION_PRIVATE 0x0488ade4
 
 extern const ecdsa_curve secp256k1;
-static int privateToPublic(unsigned char *priv_key, unsigned char *pub_key); 
-static int Base58Decode(uint8_t *in, uint8_t *out);
+static int Private_key_to_public_key(unsigned char *priv_key, unsigned char *pub_key); 
+static int Base58_decode(uint8_t *in, uint8_t *out);
+static int Seed_to_private_key(uint8_t *seed, uint8_t *pk);
 
 bool b58enc(char *b58, size_t *b58sz, const void *data, size_t binsz);
 bool b58tobin(void *bin, size_t *binszp, const char *b58);
 /***************************************************************************************/
-static int privateToPublic(uint8_t *prik, uint8_t *pubk) 
+static int Private_key_to_public_key(uint8_t *prik, uint8_t *pubk) 
 {
     curve_point R;
     bignum256 k;
@@ -27,7 +28,7 @@ static int privateToPublic(uint8_t *prik, uint8_t *pubk)
     char b58_str[32+32] = {0};
     size_t res = sizeof(b58_str);
 
-    Base58Decode(prik, priv_key_raw);
+    Base58_decode(prik, priv_key_raw);
     bn_read_be(priv_key_raw, &k);
     // compute k*G
     scalar_multiply(&secp256k1, &k, &R);
@@ -53,7 +54,7 @@ void PRINTF_ARRAY(char *tag, uint8_t *p, int len)
     printf("\n\n");
 }
 
-static int Base58Decode(uint8_t *in, uint8_t *out)
+static int Base58_decode(uint8_t *in, uint8_t *out)
 {
     uint8_t check[4] = {0};
     uint8_t hash[32 + 32] = {0};
@@ -81,35 +82,54 @@ static int Base58Decode(uint8_t *in, uint8_t *out)
     }
 
     if (memcmp(check, hash, 4) != 0) {
-        printf("check error!");
+        printf("%s: check error!", in);
         goto error;
     }
 
     memcpy(out, o + offset, key_len - offset);
-    printf("%s: %s - success!\n",__func__, in);
     return 0;
 
 error:
     return -1;
 }
 
+static int Seed_to_private_key(uint8_t *seed, uint8_t *pk) 
+{
+    uint8_t a = 0x80;
+    uint8_t hash[32+32];
+    uint8_t priv_key[33];
+    uint8_t buf[33 + 4];
+    char b58_str[64] = {0};
+    int seed_len;
+
+    printf("seed:%s\n", seed);
+
+    seed_len = strlen((char *)seed);
+    hasher_Raw(HASHER_SHA2, seed, seed_len, priv_key + 1);
+    priv_key[0] = a;
+
+    hasher_Raw(HASHER_SHA2, priv_key, 33, hash + 32);
+    hasher_Raw(HASHER_SHA2, hash + 32, 32, hash);
+
+    memcpy(buf, priv_key, 33);
+    memcpy(buf + 33, hash, 4);
+
+    size_t res = sizeof(b58_str);
+    if (b58enc(b58_str, &res, (const char *)buf, sizeof(buf))) {
+        memcpy(pk, b58_str, strlen(b58_str));
+        return 0;
+    }
+    return -1;
+}
 
 int main(void)
 {
-    uint8_t seed[] = "I love you so much!";
-    uint8_t private_key[256] = "5HqctJyXzsaSybGYxJZnKAqVPPp73AXtDiVUQ95CStTLwiJuzik";
-    uint8_t public_key[256] = {0};
+    uint8_t seed[] = "你好eos"; //"5KBGwdpPYgViff1wram2UnUCoU4eDK5eKTpZTBGRHCBgiPHF5o5"
+    uint8_t private_key[76] = {0};
+    uint8_t public_key[76] = {0};
 
-
-    /** Base58Decode((uint8_t *)"6vJk3nfmQm16naqbTwphK2Li8Bdy1Nq2Y96bBmnUkjo5qe3G8B", public_key); */
-    /** Base58Decode((uint8_t *)"5HqctJyXzsaSybGYxJZnKAqVPPp73AXtDiVUQ95CStTLwiJuzik", private_key); */
-    /** return 0; */
-
-    printf("\nseed:%s\n", seed);
-
-    privateToPublic(private_key, public_key);
-    PRINTF_ARRAY("public key", public_key, 33);
-
+    Seed_to_private_key(seed, private_key);
+    Private_key_to_public_key(private_key, public_key);
     return 0;
 }
 
